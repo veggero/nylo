9cd38856d801cb829086b1e2347a0e6720f0fbfd
Hearot <hearotdev@gmail.com>
2018-01-05 09:40:58 +0100

    Fix commands & create functions helpMessage and getCommands

--- src/Main.py
+++ src/Main.py
@@ -4,12 +4,36 @@ import platform
 import datetime
 import time
 
+
 def main(argv):
     try:
-        if argv['version']:
-            print("Nylo 0.10 (" + date(time.time()) + ")\n(" + platform.release() + ") on " + platform.system())
+        command = getCommand(argv)
+        if command == 'version':
+            print("Nylo 0.10 (" + date(time.time()) + ")\n(" +
+                  platform.release() + ") on " + platform.system())
+            exit()
+        if command == 'help':
+            print(helpMessage())
     except KeyError:
         pass
+
+
+def getCommand(parsedArguments):
+    for key, value in parsedArguments.items():
+        if key == 'version' and value:
+            return 'version'
+        elif key == 'help' and value:
+            return 'help'
+
+
+def helpMessage():
+    return ("usage: nylo [options] file\n\n" +
+            "A new programming language\n\n" +
+            "nylo options:\n      --version" +
+            "            display version" +
+            " number of nylo and platform" +
+            " details\n      --help      " +
+            "          display this help message")
 
 
 def parseArguments(argv):
@@ -29,9 +53,10 @@ def parseArguments(argv):
     return parsedArguments
 
 
-def date(unixtime, format = '%m/%d/%Y %H:%M'):
+def date(unixtime, format='%m/%d/%Y %H:%M'):
     d = datetime.datetime.fromtimestamp(unixtime)
     return d.strftime(format)
+
 
 if __name__ == '__main__':
     main(parseArguments(sys.argv))
