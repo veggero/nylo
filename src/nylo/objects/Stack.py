bbf8fea392da6a2b5a27226531ad51bf4deb5e19
veggero <niccolo.venerandi@gmail.com>
2018-04-04 01:15:48 +0200

    Even faster.

--- src/nylo/objects/Stack.py
+++ src/nylo/objects/Stack.py
@@ -1,6 +1,8 @@
 class Stack(list):
     
     def __getitem__(self, value):
+        if isinstance(value, int): return list.__getitem__(self, value)
+        if value in self[0]: return self[0].getitem(value, self)
         for element in reversed(self):
             if value in element: return element.getitem(value, self)
         raise NameError("Name '%s' is not defined" % value)
