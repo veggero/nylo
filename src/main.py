134cbcbfa92383f7eb9b361cf520f3dbb07463e3
Hearot <hearotdev@gmail.com>
2018-04-14 22:25:47 +0200

    Just re-design the main file & delete the prev_code

--- src/main.py
+++ src/main.py
@@ -1,13 +1,27 @@
 import nylo
 import sys
+import argparse
 
-with open(sys.argv[-1], 'r') as codefile: 
+if len(sys.argv) <= 1:
+    sys.argv.append('-h')
+sys.argv = sys.argv[1:]
+
+parser = argparse.ArgumentParser(description='A cool programming language')
+parser.add_argument('-f', '--file',
+                     help='the file you want to evaluate')
+parser.add_argument('-v', '--version',
+                     help='print current version',
+                     action='version',
+                     version='nylo 0.1')
+args = parser.parse_args(sys.argv)
+
+with open(args.file, 'r') as codefile:
     code = codefile.read()
 
 reader = nylo.Reader(code)
 struct = nylo.Struct(reader).value
 
-#struct.settype(['obj'], nylo.nyglobals)
+# struct.settype(['obj'], nylo.nyglobals)
 
 print(struct.calculate(nylo.nyglobals))
 
