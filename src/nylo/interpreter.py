6f6151ee47b3d2172a3a479514ae5b482856c246
veggero <niccolo.venerandi@gmail.com>
2018-07-19 00:58:29 +0200

    Added structure parser.

--- src/nylo/interpreter.py
+++ src/nylo/interpreter.py
@@ -11,3 +11,11 @@ class Variable(str):
     
     def __repr__(self):
         return '$%s' % self
+
+class Struct:
+
+    def __init__(self, dirs, atoms, value):
+        self.dirs, self.atoms, self.value = dirs, atoms, value
+        
+    def __repr__(self):
+        return "%s/%s/%s" % (self.dirs, self.atoms, self.value)
--- src/nylo/parser.py
+++ src/nylo/parser.py
@@ -14,8 +14,70 @@ from string import ascii_lowercase as letters, digits
 
 
 from exceptions import NySyntaxError
-from interpreter import Integer, Float, String, Variable
+from interpreter import Integer, Float, String, Variable, Struct
 
+
+def structure(code: list):
+    """
+    struct ::= "(" ((value | value ":" value) [","])* ("->" value)? ")"
+    >>> structure([*'(a:  1  5  :b   c  3  d  ->   5)'])
+    {$a: 1, 5: $b}/($c, 3, $d)/5
+    >>> structure([*'(  5  1:1, , 6:b 6 , 8 d: f  ,->10)'])
+    {1: 1, 6: $b, $d: $f}/(5, 6, 8)/10
+    >>> structure([*'(r g b)'])
+    {}/($r, $g, $b)/None
+    >>> structure([])
+    Traceback (innermost last):
+      ....
+    exceptions.NySyntaxError: Non-structure character while parsing structure.
+    >>> structure([*'(2, 4 b'])
+    Traceback (innermost last):
+      ....
+    exceptions.NySyntaxError: Unexpected EOF while parsing.
+    >>> structure([*'(2, 4 b,,'])
+    Traceback (innermost last):
+      ....
+    exceptions.NySyntaxError: Unexpected EOF while parsing.
+    >>> structure([*'(2, 4 b: '])
+    Traceback (innermost last):
+      ....
+    exceptions.NySyntaxError: Unexpected EOF while parsing.
+    >>> structure([*'(2, 4 -> 3'])
+    Traceback (innermost last):
+      ....
+    exceptions.NySyntaxError: Unexpected EOF while parsing.
+    >>> structure([*'(1, 4, -> 2 4)'])
+    Traceback (innermost last):
+      ....
+    exceptions.NySyntaxError: Missing ")" after "->" value.
+    """
+    if not code or not code[0] == '(':
+        raise NySyntaxError('Non-structure character while parsing structure.')
+    code.pop(0)
+    dirs, atoms, rvalue = {}, [], None
+    while code[0] != ')':
+        while code and code[0] == ',':
+            code.pop(0)
+            whitespace(code)
+        if ''.join(code).startswith('->'):
+            del code[:2]
+            rvalue = anyvalue(code)
+            break
+        value = anyvalue(code)
+        if not code:
+            raise NySyntaxError('Unexpected EOF while parsing.')
+        if code[0] == ':':
+            code.pop(0)
+            key, value = value, anyvalue(code)
+            dirs[key] = value
+        else:
+            atoms.append(value)
+    if not code:
+        raise NySyntaxError('Unexpected EOF while parsing.')
+    elif code[0] != ')':
+        raise NySyntaxError('Missing ")" after "->" value.')
+    code.pop(0)
+    return Struct(dirs, tuple(atoms), rvalue)
 
 def anyvalue(code: list):
     """
@@ -30,19 +92,23 @@ def anyvalue(code: list):
     >>> anyvalue([*'~'])
     Traceback (most recent call last):
       ...
-    exceptions.NySyntaxError: Unexpected character found while parsing.
+    exceptions.NySyntaxError: Unexpected character "'~'" found while parsing.
     >>> anyvalue([])
     Traceback (most recent call last):
       ...
     exceptions.NySyntaxError: Unexpected EOF while parsing.
     """
+    whitespace(code)
     try:
-        return next(f(code) for v, f in {digits + '.': number,
-            '"\'«': string, letters + '_': variable}.items() if code[0] in v)
+        value = next(f(code) for v, f in {digits + '.': number,
+            '"\'«': string, letters + '_': variable,
+            '(': structure}.items() if code[0] in v)
     except StopIteration:
-        raise NySyntaxError('Unexpected character found while parsing.')
+        raise NySyntaxError('Unexpected character "%s" found while parsing.' % repr(code[0]))
     except IndexError:
         raise NySyntaxError('Unexpected EOF while parsing.')
+    whitespace(code)
+    return value
     
     
 def whitespace(code: list, indent=[0]):
@@ -100,7 +166,7 @@ def whitespace(code: list, indent=[0]):
 
 def number(code: list) -> Union[Integer, Float]:
     """
-    number ::= (1-9)* ["." (1-9)*]
+    number ::= (1-9)* ("." (1-9)*)?
     >>> number([*'123'])
     123
     >>> number([*'1 2 3'])
