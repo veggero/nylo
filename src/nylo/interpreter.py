4c51b605a3510046d3fe7b64bfc4223d1ed38224
veggero <niccolo.venerandi@gmail.com>
2018-08-27 09:17:34 +0200

    wip

--- src/nylo/interpreter.py
+++ src/nylo/interpreter.py
@@ -4,13 +4,21 @@ class Integer(int):
 class Float(float):
     pass
 
-class String(str):
+class Text(str):
     pass
 
 class Variable(str):
     
     def __repr__(self):
         return '$%s' % self
+    
+class Op:
+    
+    def __init__(self, op, args):
+        self.op, self.args = op, args
+        
+    def __repr__(self):
+        return '(%s)(%s)' % (self.op, ' '.join(map(repr, self.args)))
 
 class Struct:
 
--- src/nylo/parser.py
+++ src/nylo/parser.py
@@ -14,61 +14,35 @@ from string import ascii_lowercase as letters, digits
 
 
 from exceptions import NySyntaxError
-from interpreter import Integer, Float, String, Variable, Struct
+from interpreter import Integer, Float, Text, Variable, Struct, Op
 
 
 def structure(code: list):
     """
     struct ::= "(" ((value | value ":" value) [","])* ("->" value)? ")"
-    >>> structure([*'(a:  1  5  :b   c  3  d  ->   5)'])
-    {$a: 1, 5: $b}/($c, 3, $d)/5
-    >>> structure([*'(  5  1:1, , 6:b 6 , 8 d: f  ,->10)'])
-    {1: 1, 6: $b, $d: $f}/(5, 6, 8)/10
-    >>> structure([*'(r g b)'])
-    {}/($r, $g, $b)/None
-    >>> structure([])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Non-structure character while parsing structure.
-    >>> structure([*'(2, 4 b'])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Unexpected EOF while parsing.
-    >>> structure([*'(2, 4 b,,'])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Unexpected EOF while parsing.
-    >>> structure([*'(2, 4 b: '])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Unexpected EOF while parsing.
-    >>> structure([*'(2, 4 -> 3'])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Unexpected EOF while parsing.
-    >>> structure([*'(1, 4, -> 2 4)'])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Missing ")" after "->" value.
     """
     if not code or not code[0] == '(':
         raise NySyntaxError('Non-structure character while parsing structure.')
     del code[0]
-    dirs, atoms, rvalue = {}, [], None
+    dirs, atoms, rvalue, hascomma = {}, [], None, False
     while code[0] != ')':
-        while code and code[0] == ',':
+        if code and code[0] == ',':
             del code[0]
+            hascomma = True
             whitespace(code)
+            if not code:
+                raise NySyntaxError('Unexpected EOF while parsing.')
+            continue
         if ''.join(code).startswith('->'):
             del code[:2]
-            rvalue = anyvalue(code)
+            rvalue = sentence(code)
             break
-        value = anyvalue(code)
+        value = sentence(code)
         if not code:
             raise NySyntaxError('Unexpected EOF while parsing.')
         if code[0] == ':':
             del code[0]
-            key, value = value, anyvalue(code)
+            key, value = value, sentence(code)
             dirs[key] = value
         else:
             atoms.append(value)
@@ -77,79 +51,39 @@ def structure(code: list):
     elif code[0] != ')':
         raise NySyntaxError('Missing ")" after "->" value.')
     del code[0]
+    if not dirs and not rvalue and len(atoms) == 1 and not hascomma:
+        return atoms[0]
     return Struct(dirs, tuple(atoms), rvalue)
+
+
+def sentence(code: list):
+    """
+    sentence ::= anyvalue? symbol? sentence?
+    """
+    if not code:
+        raise NySyntaxError('Unexpected EOF while parsing.')
+    value, symbol, sentence = None, None, None
+    if code[0] in (el for l in parsers.keys() for el in l):
+        value = anyvalue(code)
+        #TODO
+
 
 def anyvalue(code: list):
     """
     anyvalue ::= number | string | variable
-    >>> code = [*'«strakas»19sailors']
-    >>> anyvalue(code)
-    'strakas'
-    >>> anyvalue(code)
-    19
-    >>> anyvalue(code)
-    $sailors
-    >>> anyvalue([*'~'])
-    Traceback (most recent call last):
-      ...
-    exceptions.NySyntaxError: Unexpected character "'~'" found while parsing.
-    >>> anyvalue([])
-    Traceback (most recent call last):
-      ...
-    exceptions.NySyntaxError: Unexpected EOF while parsing.
     """
     whitespace(code)
     try:
-        value = next(f(code) for v, f in {digits + '.': number,
-            '"\'«': string, letters + '_': variable,
-            '(': structure}.items() if code[0] in v)
+        value = next(f(code) for v, f in parsers.items() if code[0] in v)
     except StopIteration:
         raise NySyntaxError('Unexpected character "%s" found while parsing.' % repr(code[0]))
     except IndexError:
         raise NySyntaxError('Unexpected EOF while parsing.')
     whitespace(code)
     return value
-    
-    
+
+        
 def whitespace(code: list, indent=[0]):
-    r"""
-    >>> code = [*'    ']
-    >>> whitespace(code)
-    >>> code
-    []
-    >>> whitespace([*" \t \t 3"])
-    Traceback (most recent call last):
-      ...
-    exceptions.NySyntaxError: Unexpected tab while parsing whitespace.
-    >>> code = []
-    >>> whitespace(code)
-    >>> code
-    []
-    >>> code = [*"\n 3"]
-    >>> whitespace(code, [0])
-    >>> code
-    ['(', '3']
-    >>> code = [*"\n\t3"]
-    >>> whitespace(code, [1])
-    >>> code
-    [',', '3']
-    >>> code = [*"\n 3"]
-    >>> whitespace(code, [2])
-    >>> code
-    [')', '3']
-    >>> code = [*"\n\t3"]
-    >>> whitespace(code, [3])
-    >>> code
-    [')', ')', '3']
-    >>> code = [*'\n']
-    >>> whitespace(code, [4])
-    >>> code
-    [')', ')', ')', ')']
-    >>> code = [*"\n\t\t\t\n\t3"]
-    >>> whitespace(code, [2])
-    >>> code
-    [')', '3']
-    """
     deque(map(code.remove, [*takewhile(lambda x: x == ' ', code)]))
     if code and code[0] == '\t':
         raise NySyntaxError("Unexpected tab while parsing whitespace.")
@@ -167,20 +101,6 @@ def whitespace(code: list, indent=[0]):
 def number(code: list) -> Union[Integer, Float]:
     """
     number ::= (1-9)* ("." (1-9)*)?
-    >>> number([*'123'])
-    123
-    >>> number([*'1 2 3'])
-    1
-    >>> number([*'42.2.'])
-    42.2
-    >>> number([])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Non-numeric character while parsing number.
-    >>> number([*'caffe'])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Non-numeric character while parsing number.
     """
     value: str = ''.join(takewhile(str.isdigit, code))
     if not value:
@@ -191,56 +111,41 @@ def number(code: list) -> Union[Integer, Float]:
     return (Float if '.' in value else Integer)(value)
 
 
-def string(code: list) -> String:
+def text(code: list) -> Text:
     """
-    string ::= '"' .* '"' | "'" .* "'" | "«" .* "»"
-    >>> string([*"'spam'"])
-    'spam'
-    >>> string([*'"foo" "bar"'])
-    'foo'
-    >>> string([*'«42»!'])
-    '42'
-    >>> string([*'caffe'])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Non-string character while parsing string.
-    >>> string([])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Non-string character while parsing string.
-    >>> string([*'«this has no end'])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Found EOF while parsing for string end.
+    text ::= '"' .* '"' | "'" .* "'" | "«" .* "»"
     """
     if not (code and code[0] in '\'"«'):
-        raise NySyntaxError('Non-string character while parsing string.')
+        raise NySyntaxError('Non-text character while parsing text.')
     try:
-        return String(''.join(iter(partial(code.pop, 0),
+        return Text(''.join(iter(partial(code.pop, 0),
             {"'": "'", '"': '"', '«': '»'}[code.pop(0)])))
     except IndexError:
-        raise NySyntaxError('Found EOF while parsing for string end.')
+        raise NySyntaxError('Found EOF while parsing for text end.')
 
 def variable(code: list) -> Variable:
     """
     variable ::= (a-Z_) (a-Z_')*
-    >>> variable([*'tau'])
-    $tau
-    >>> variable([*"k_9' leela"])
-    $k_9'
-    >>> variable([*'c8e.'])
-    $c8e
-    >>> variable([*'32'])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Non-variable character while parsing variable.
-    >>> variable([])
-    Traceback (innermost last):
-      ....
-    exceptions.NySyntaxError: Non-variable character while parsing variable.
     """
     if not (code and code[0] in letters + '_'):
         raise NySyntaxError('Non-variable character while parsing variable.')
     value = ''.join(takewhile(lambda x: x in letters + digits + "_'", code))
     del code[:len(value)]
     return Variable(value)
+
+
+symbols_priority = (
+    ('|',), ('and ', 'or ', 'xor '),
+    ('=', '!=', '>=', '<=', 'in ', '>', '<'),
+    ('..', '%'), ('+', '-', '&'),
+    ('*', '/'), ('^', '+-'), ('.',))
+
+
+symbols = tuple(sorted((f for a in symbols_priority for f in a),
+                    key=len, reverse=True))
+
+
+parsers = {digits + '.': number,
+            '"\'«': text, 
+            letters + '_': variable,
+            '(': structure,}
