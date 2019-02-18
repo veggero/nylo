aee7a8c0829755f730327af7e93b8411dc7e041b
veggero <niccolo@venerandi.com>
2019-02-18 14:38:38 +0100

    Fixed memory leak

--- nylo/__main__.py
+++ nylo/__main__.py
@@ -4,12 +4,27 @@ from parser import Parser, newParser
 from code import Code
 from mesh import newMesh
 from writer import Writer
+import time
 
 if not len(sys.argv) - 1:
 	exit('usage: nylo file.ny')
 	
 this, target = sys.argv
 name = target.partition('/')[2].partition('.')[0]
+
+first_before = time.time()
+
+std_parser = Parser(Code('('+open('std/base.ny', 'r').read()+')'))
+std_parser.parse(('base',))
+parser = Parser(Code('('+open(target, 'r').read()+')'))
+parser.parse(('base', name,))
+parser.mesh.update(std_parser.mesh)
+parser.mesh.bind()
+writer = Writer(parser.mesh)
+print(writer.write(('base', name, 'self')))
+
+second_before = time.time()
+
 
 std_parser = newParser(Code('('+open('std/base.ny', 'r').read()+')'))
 parser = newParser(Code('('+open(target, 'r').read()+')'))
@@ -18,25 +33,12 @@ obj = parser.parse(('base', name))
 std_obj[1]['base'][1][name] = obj
 nM = newMesh(std_obj)
 nM.obj = nM.bind()
-
-#print(nM.valueof(('base', name, 'self')))
-
-#pprint(nM.obj[1]['base'][1]['if'])
-
 writer = Writer(nM)
 print(writer.write(('base', name, 'self')))
+after = time.time()
 
-#std_parser = newParser(Code('('+open('std/base.ny', 'r').read()+')'))
-#o = std_parser.parse(('base',))
-#std_parser.convert(o, ('base',))
+old = round(second_before-first_before, 2)
+new = round(after-second_before, 2)
+ratio = round(old/new, 2)
 
-#parser = newParser(Code('('+open(target, 'r').read()+')'))
-#o = parser.parse(('base', name,))
-#parser.convert(o, ('base', name))
-#parser.mesh.update(std_parser.mesh)
-#parser.mesh.bind()
-#writer = Writer(parser.mesh)
-
-#print("size in >", len(parser.mesh), sys.getsizeof(parser.mesh)/1000)
-#print(writer.write(('base', name, 'self')))
-#print("size out <", len(parser.mesh), sys.getsizeof(parser.mesh)/1000)
+print(f'\nNew: {new}s\nOld: {old}s\nRatio: {ratio}')
